67967bb87f564115baec796893755e2fb8f60fe6
Zoltan Buka <bukazoltan@gmail.com>
2019-11-29 11:44:48 +0100

    arrays in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,83 @@
 "use strict";
 
-var add = function add(a, b) {
-  return a + b;
+console.log('App.js is running!');
+
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer",
+  options: ["one", "two"]
 };
 
-console.log(add(55, 1));
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Here are your options:" : "No options!"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    )
+  )
+);
 
 var user = {
-  name: 'Andrew',
-  cities: ["Budapest", "New York", "Dublin"],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    });
-  }
+  name: "Zoltán",
+  age: 27,
+  location: "Budapest"
 };
 
-console.log(user.printPlacesLived());
-
-var multiplier = {
-  numbers: [12, 20, 100],
-  multiplyBy: 3,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      "p",
+      null,
+      "Location: ",
+      location
+    );
+  } else {
+    return undefined;
   }
-};
+}
 
-console.log(multiplier.multiply());
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name ? user.name : 'Anonymus'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  getLocation(user.location)
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,45 +1,51 @@
 console.log('App.js is running!');
 
 const app = {
-  title: "Indecision App",
-  subtitle: "Put your life in the hands of a computer",
-  options: ["one", "two"]
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
 };
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? "Here are your options:": "No options!"}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item one</li>
-    </ol>
-  </div>
-);
+const onFormSubmit = (e) => {
+  e.preventDefault();
 
-const user = {
-  name: "Zoltán",
-  age: 27,
-  location: "Budapest"
-};
+  const option = e.target.elements.option.value;
 
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  } else {
-    return undefined;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
   }
-}
+};
 
-const templateTwo = (
-  <div>
-    <h1>{user.name ? user.name : 'Anonymus'}</h1>
-    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
-  </div>
-);
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
 
-const appRoot = document.getElementById("app");
+const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option) => <li key={option}>{option}</li>)
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
